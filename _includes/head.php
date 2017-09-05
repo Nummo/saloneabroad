fe2799dda60aecfe2bfc309833c92fd5cdb25f95
Nummo <Nenna@Nennas-MacBook-Pro.local>
2017-09-05 18:56:23 +0100

    added folders, trials with php

---
+++ _includes/head.php
@@ -0,0 +1,5 @@
+<?php
+echo '<meta name="viewport" ccontent="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="index.css">
+		<title>Salone Abroad</title>';
+?>
---
+++ _layouts/menu.php
@@ -0,0 +1,8 @@
+<?php
+echo '<a href="#home"><img src="img/salogo@3x.png" height="30" width="30" align="middle">Salone Abroad</a>
+  	<a href="#about">About</a>
+ 	<a href="#projects">Projects</a>
+  	<a href="#contact">Contact Us</a>
+  	<a href="#community">Community</a>
+  	<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>';
+?>
